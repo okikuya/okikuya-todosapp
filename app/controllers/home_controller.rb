eb8a645e8adee89990e6db1fddb883a319d97bf5
okikuya <noilfo3hcramosa@gmail.com>
2025-03-08 21:48:59 +0900

    ボード一覧からボードを更新・削除できる

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,5 @@
 class HomeController < ApplicationController
     def index
-        @boards = current_user.boards
-        # @boards = Board.includes(:user).all
+        @boards = Board.includes(:user).all
     end
 end
